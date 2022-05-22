9a4ac69ec4ddc0aae927a032ed0d6b410b10625e
Michael Tkachenko <20werasdf@gmail.com>
2022-05-22 11:54:05 +0300

    some work

--- Large_Numbers/Program.cs
+++ Large_Numbers/Program.cs
@@ -1,4 +1,133 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Text;
 
-Console.WriteLine("Changed Branch");
+Task.Factory.StartNew(() =>
+{
+    var template1 = new List<string>()
+    {
+        "00",
+        "01",
+        "10",
+        "11"
+    };
+
+    var bitsAmount = 32;
+    var expectedSequences = (int)Math.Pow(2, bitsAmount);
+
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    Calculate1(template1, expectedSequences);
+
+    TimeSpan ts = stopWatch.Elapsed;
+    var runtime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+    Console.WriteLine($"runtime of : {nameof(Calculate1)}" + runtime);
+});
+
+Task.Factory.StartNew(() =>
+{
+    var template2 = new List<string>()
+    {
+        "00",
+        "01",
+        "10",
+        "11"
+    };
+
+    var bitsAmount = 32;
+    var expectedSequences = (int)Math.Pow(2, bitsAmount);
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    Calculate2(template2, expectedSequences);
+
+    TimeSpan ts = stopWatch.Elapsed;
+    var runtime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+    Console.WriteLine($"runtime of : {nameof(Calculate2)}" + runtime);
+});
+
+
+//foreach (var line in result)
+//{
+//    //var halfByte = 4;
+//    //var halfBytes = line.Length / 4;
+
+//    //for (int i = 1; i < halfBytes + 1; i++)
+//    //{
+//    //    var t = line.Take(halfByte * i);
+//    //}
+
+//    Console.WriteLine(line);
+//}
+
+//Console.WriteLine(result.Count);
+
+Console.ReadKey();
+
+static List<string> Calculate1(List<string> s, int num)
+{
+    var buffer = new List<string>();
+
+    Action<string> addTop = (string first) =>
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < s.Count; i++)
+        {
+            var part = sb.AppendJoin("", first, s[i]).ToString();
+            sb.Clear();
+            buffer.Add(part);
+        }
+    };
+
+    addTop("0");
+    addTop("1");
+
+    s.Clear();
+
+    s = s.Concat(buffer).ToList();
+
+    if (s.Count < num)
+        return Calculate1(s, num);
+
+    return s;
+}
+
+static List<string> Calculate2(List<string> s, int num)
+{
+    var buffer = new List<string>();
+
+    Action<string> addTop = (string first) =>
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < s.Count; i++)
+        {
+            var part = sb.AppendJoin("", first, s[i]).ToString();
+            sb.Clear();
+            buffer.Add(part);
+        }
+    };
+
+    addTop("0");
+    addTop("1");
+
+    s.Clear();
+
+    //s = s.Concat(buffer).ToList();
+
+    foreach (var temp in buffer)
+        s.Add(temp);
+
+    if (s.Count < num)
+        return Calculate2(s, num);
+
+    return s;
+}
+
