ed76cee2751e96815d10a6d14d17013135b2de69
Michael Tkachenko <20werasdf@gmail.com>
2022-05-23 16:16:48 +0300

    tree created

--- Large_Numbers/Program.cs
+++ Large_Numbers/Program.cs
@@ -1,85 +1,108 @@
 ﻿using System.Text;
 
-var template = new List<string>()
-{
-    "00",
-    "01",
-    "10",
-    "11"
-};
 
-var bitsAmount = 16;
-var expectedSequences = (int)Math.Pow(2, bitsAmount);
+var tree = new BinaryTree("0", 4);
+tree.BuildTree();
 
-var task = CalculateAsync(template, expectedSequences);
+tree.PrintTree();
 
-var loadingWord = "Loading";
-
-Console.CursorVisible = false;
-Console.Write(loadingWord);
-
-while (!task.IsCompleted)
-{
-    Console.SetCursorPosition(loadingWord.Length, 0);
-
-    for (int i = 0; i < 4; i++)
-    {
-        Thread.Sleep(1000);
-        Console.Write(".");
-    }
-
-    Console.SetCursorPosition(loadingWord.Length,0);
-
-    for (int i = 0; i < 4; i++)
-        Console.Write(" ");
-}
-
-var resultList = task.Result;
-
-Console.Clear();
-
-foreach (var line in resultList)
-    Console.WriteLine(line);
-
-Console.WriteLine(resultList.Count);
+Console.WriteLine();
+Console.WriteLine(tree.NodesAmount);
 
 Console.ReadKey();
 
-static Task<List<string>> CalculateAsync(List<string> s, int num)
+class Node
 {
-    return Task.Run(() => Calculate(s, num));
-}
+    public string Data { get; set; }
+    public Node LeftChild { get; set; }
+    public Node RightChild { get; set; }
+    public Node Parent { get; set; }
 
-static List<string> Calculate(List<string> s, int num)
-{
-    var buffer = new List<string>();
-
-    Action<string> addTop = (string first) =>
+    public void AddRight()
     {
-        var sb = new StringBuilder();
+        RightChild = new Node() { Data = "1", Parent = this };
+    }
+    public void AddLeft()
+    {
+        LeftChild = new Node() { Data = "0", Parent = this };
+    }
 
-        for (int i = 0; i < s.Count; i++)
-        {
-            var part = sb.AppendJoin("", first, s[i]).ToString();
-            sb.Clear();
-            buffer.Add(part);
-        }
-    };
-
-    addTop("0");
-    addTop("1");
-
-    s.Clear();
-
-    s = s.Concat(buffer).ToList();
-
-    return s.Count < num ? Calculate(s, num) : s;
+    public override string ToString()
+    {
+        return $"{LeftChild.Data} <- {Data} -> {RightChild.Data}";
+    }
 }
 
-//var halfByte = 4;
-//var halfBytes = line.Length / 4;
+class BinaryTree
+{
+    public Node Root { get; private set; }
+    public int TreeDepth { get; private set; }
 
-//for (int i = 1; i < halfBytes + 1; i++)
-//{
-//    var t = line.Take(halfByte * i);
-//}
+    private int _currentDepth = 0;
+    private List<Node> markedNodes = new List<Node>();
+    public int NodesAmount { get; set; } = 0;
+
+    public BinaryTree(string data, int keyLength)
+    {
+        Root = new Node() { Data = data };
+        TreeDepth = keyLength - 1;
+        NodesAmount++;
+    }
+
+    public void BuildTree()
+    {
+        var current = Root;
+        Do(current);
+        _currentDepth = 0;
+    }
+
+    public void PrintTree()
+    {
+        PrintNode(Root);
+        _currentDepth = 0;
+        markedNodes.Clear();
+    }
+
+    public void Do(Node current)
+    {
+        if (_currentDepth == TreeDepth)
+            return;
+
+        if (current.LeftChild == null)
+        {
+            current.AddLeft();
+            _currentDepth++;
+            NodesAmount++;
+            Do(current.LeftChild);
+            _currentDepth--;
+        }
+            
+        if (current.RightChild == null)
+        {
+            current.AddRight();
+            NodesAmount++;
+            _currentDepth++;
+            Do(current.RightChild);
+            _currentDepth--;
+        }
+    }
+
+    public void PrintNode(Node current) // just to check the tree
+    {
+        if (current == null)
+            return;
+
+        for (int i = 0; i < _currentDepth; i++)
+        {
+            Console.Write("-");
+        }
+        Console.WriteLine(current.Data);
+
+        _currentDepth++;
+        PrintNode(current.LeftChild);
+        _currentDepth--;
+        _currentDepth++;
+        PrintNode(current.RightChild);
+        _currentDepth--;
+    }
+}
